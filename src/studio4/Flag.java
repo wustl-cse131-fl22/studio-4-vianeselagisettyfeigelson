825ee598901b7422665701ba50684112a303e4aa
wvianese <wvianese@localhost>
2022-10-13 14:09:22 -0500

    done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,13 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		Color outerRec = new Color(164, 240, 156);
+		StdDraw.setPenColor(outerRec);
+		StdDraw.filledRectangle(.5, .5, .4, .4);
+		
+		Color innerEllipse = new Color(143, 127, 245);
+		StdDraw.setPenColor(innerEllipse);
+		StdDraw.filledEllipse(.5, .5, .2, .2);
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,41 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		Color rando = new Color(red, green, blue);
+		
+		if (shape.equals ("ellipse")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfx = in.nextDouble();
+			double halfy = in.nextDouble();
+			if (isFilled == true) {
+				StdDraw.setPenColor(rando);
+				StdDraw.filledEllipse(x, y, halfx, halfy);
+			}
+			else  {
+				StdDraw.setPenColor(rando);
+				StdDraw.ellipse(x, y, halfx, halfy);
+			}
+		}
+		
+		if (shape.equals ("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfx = in.nextDouble();
+			double halfy = in.nextDouble();
+			if (isFilled == true) {
+				StdDraw.setPenColor(rando);
+				StdDraw.filledRectangle(x, y, halfx, halfy);
+			}
+			else {
+				StdDraw.setPenColor(rando);
+				StdDraw.rectangle(x, y, halfx, halfy);
+			}
+		}	
 	}
 }
